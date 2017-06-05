377ffc93841eb178ece9c09954e92de845ebd22d
guijianliang <guijianliang@163.com>
2017-06-05 09:39:35 +0800

    ceshi

---
+++ src/main/java/com/helloworld/HelloworldTest.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+/**
+ * Created by guijianliang on 2017/6/3.
+ */
+public class HelloworldTest {
+    public static void main(String[] args) {
+        System.out.println("你好，桂建亮。");
+    }
+}
